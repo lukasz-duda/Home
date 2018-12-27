c8cd99a6426dffaaa3747a9cacf871bf368ec622
Łukasz Duda <lukasz.duda.pl@gmail.com>
2018-12-27 23:50:12 +0100

    Content type header.

--- cat/test_connection.php
+++ cat/test_connection.php
@@ -4,4 +4,6 @@ class CommandResponse {
 	public $information = 'Połączenie z API zostało nawiązane poprawnie.';
 }
 
+header('Content-Type: application/json');
+
 echo json_encode(new CommandResponse());
