2a10a5eaa53fb81c94bd3bf1934bcbd4e316e939
Łukasz Duda <lukasz.duda.pl@gmail.com>
2018-12-27 23:57:47 +0100

    Charset.

--- cat/test_connection.php
+++ cat/test_connection.php
@@ -4,6 +4,6 @@ class CommandResponse {
 	public $information = 'Połączenie z API zostało nawiązane poprawnie.';
 }
 
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=UTF-8');
 
 echo json_encode(new CommandResponse());
