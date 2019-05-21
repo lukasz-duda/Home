54ab1bf189f5aeaf600c7e22c15fbaea788ef54d
Łukasz Duda <lukasz.duda.pl@gmail.com>
2019-05-21 16:57:50 +0200

    Validation. Hot fix.

--- src/CatFeeding/Application/StartMealController.php
+++ src/CatFeeding/Application/StartMealController.php
@@ -6,6 +6,11 @@ $catId = 1;
 $foodId = intval($_REQUEST['FoodId']);
 $weight = intval($_REQUEST['Weight']);
 
+if ($foodId == 0) {
+    echo 'Nie wybrano pokarmu. Spróbuj jeszcze raz.';
+    return;
+}
+
 $saveMeal = $pdo->prepare('INSERT INTO meal (cat_id, food_id, start, start_weight) values (?, ?, ?, ?)');
 $mealSaved = $saveMeal->execute([$catId, $foodId, date('Y-m-d H:i:s'), $weight]);
 
