239bc696f2dd4c6fce519f2dc346173e6e271517
Łukasz Duda <lukasz.duda.pl@gmail.com>
2019-05-23 16:47:45 +0200

    Expense categories.

--- src/Shopping/Infrastructure/database.sql
+++ src/Shopping/Infrastructure/database.sql
@@ -1,10 +1,18 @@
-drop table if exists expense;
+drop table if exists expenses;
 
-create table expense
+create table expenses
 (
     id        int unsigned  not null primary key auto_increment,
     timestamp datetime      not null,
     value     decimal(8, 2) not null,
     name      varchar(50)   not null,
     category  varchar(50)   not null
-)
+);
+
+drop table if exists expense_categories;
+
+create table expense_categories
+(
+    id   int unsigned not null primary key auto_increment,
+    name varchar(50)  not null
+);
