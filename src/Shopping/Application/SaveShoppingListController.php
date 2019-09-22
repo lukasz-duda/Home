5cb22a9cfbda0f312bdf57dc63d2f093ba56592d
Łukasz Duda <lukasz.duda.pl@gmail.com>
2019-09-22 21:58:51 +0200

    Validate shopping list.

--- src/Shopping/Application/SaveShoppingListController.php
+++ src/Shopping/Application/SaveShoppingListController.php
@@ -3,6 +3,11 @@ include '../../Shared/Views/View.php';
 
 $shoppingList = $_REQUEST['ShoppingList'];
 
+if (json_decode($shoppingList) === null) {
+    showError('Lista zakupów uszkodzona! Prześlij ponownie.');
+    return;
+}
+
 $saveShoppingList = $pdo->prepare('update shopping_list set json = ?');
 $shoppingListSaved = $saveShoppingList->execute([$shoppingList]);
 
