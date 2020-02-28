a9f4faa64f4fca706e09f69bce1754e7a6811087
Łukasz Duda <lukasz.duda.pl@gmail.com>
2020-02-28 16:55:59 +0100

    Correct title in case of multiple headers

--- src/Shared/Views/Footer.php
+++ src/Shared/Views/Footer.php
@@ -4,7 +4,7 @@ include_once __DIR__ . '/View.php';
     <script src="<?= $baseUrl ?>/src/Shared/Views/popper.min.js"></script>
     <script src="<?= $baseUrl ?>/src/Shared/Views/bootstrap.min.js"></script>
     <script>
-        document.title = $('h1').text();
+        document.title = $('h1').first().text();
     </script>
 <?php
 
