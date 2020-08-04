59b58496b3ba68dc94a9e291aa9fea1ea625fde7
Łukasz Duda <lukasz.duda.pl@gmail.com>
2020-08-04 16:32:19 +0200

    Default title

--- src/Shared/Views/Footer.php
+++ src/Shared/Views/Footer.php
@@ -4,7 +4,7 @@ include_once __DIR__ . '/View.php';
     <script src="<?= $baseUrl ?>/src/Shared/Views/popper.min.js"></script>
     <script src="<?= $baseUrl ?>/src/Shared/Views/bootstrap.min.js"></script>
     <script>
-        document.title = $('h1').first().text();
+        document.title = $('h1').first().text() || 'Dom';
     </script>
     <a class="btn btn-light material-icons-outlined" href="<?= $_SERVER['HTTP_REFERER'] ?>">arrow_back</a>
 <?php
