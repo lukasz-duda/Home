780ddad883f154f17c385426fefff25966b4d851
Łukasz Duda <lukasz.duda.pl@gmail.com>
2020-08-04 16:18:14 +0200

    Validate end weight

--- src/CatFeeding/Application/EndMealController.php
+++ src/CatFeeding/Application/EndMealController.php
@@ -2,16 +2,23 @@
 include '../../Shared/Views/View.php';
 
 $mealId = intval($_REQUEST['MealId']);
-$weight = intval($_REQUEST['Weight']);
+$endWeight = intval($_REQUEST['Weight']);
 
-$saveMeal = $pdo->prepare('UPDATE meal SET end = ?, end_weight = ? where id = ?');
-$mealSaved = $saveMeal->execute([date('Y-m-d H:i:s'), $weight, $mealId]);
+$meal = get('SELECT m.start_weight FROM meal m WHERE id = ?', [$mealId]);
+$startWeight = $meal['start_weight'];
 
-if ($mealSaved) {
-    showInfo('Posiłek zakończony.');
+if ($endWeight > $startWeight) {
+    showWarning('Waga nie może być większa niż na początku posiłku.');
 } else {
-    showError('Nie udało się zakończyć posiłku!');
-    showStatementError($saveMeal);
+    $saveMeal = $pdo->prepare('UPDATE meal SET end = ?, end_weight = ? where id = ?');
+    $mealSaved = $saveMeal->execute([date('Y-m-d H:i:s'), $endWeight, $mealId]);
+
+    if ($mealSaved) {
+        showInfo('Posiłek zakończony.');
+    } else {
+        showError('Nie udało się zakończyć posiłku!');
+        showStatementError($saveMeal);
+    }
 }
 
 include '../../Shared/Views/Footer.php';
