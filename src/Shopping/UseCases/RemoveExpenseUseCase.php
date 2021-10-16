808d7707f4558f2ff2dc372f0983eca044359523
Łukasz Duda <lukasz.duda.pl@gmail.com>
2021-10-16 18:11:25 +0200

    Remove refund before removing expense
    Prevent foreign key reference constraint.

--- src/Shopping/UseCases/RemoveExpenseUseCase.php
+++ src/Shopping/UseCases/RemoveExpenseUseCase.php
@@ -8,24 +8,24 @@ if (notValidId($id)) {
     return;
 }
 
+$hasRefund = get('SELECT expense_id FROM refund_plan WHERE expense_id = ?', [$id]);
+
+if ($hasRefund) {
+    $removeRefund = pdo()->prepare('DELETE FROM refund_plan WHERE expense_id = ?');
+    $refundRemoved = $removeRefund->execute([$id]);
+    if ($refundRemoved) {
+        showInfo('Zaplanowany zwrot usunięty.');
+    } else {
+        showError('Nie udało się usunąć zaplanowanego zwrotu!');
+        showStatementError($removeRefund);
+    }
+}
+
 $remove = pdo()->prepare('DELETE FROM expenses WHERE id = ?');
 $removed = $remove->execute([$id]);
 
 if ($removed) {
     showInfo('Zakup usunięty.');
-
-    $hasRefund = get('SELECT expense_id FROM refund_plan WHERE expense_id = ?', [$id]);
-
-    if ($hasRefund) {
-        $removeRefund = pdo()->prepare('DELETE FROM refund_plan WHERE expense_id = ?');
-        $refundRemoved = $removeRefund->execute([$id]);
-        if ($refundRemoved) {
-            showInfo('Zaplanowany zwrot usunięty.');
-        } else {
-            showError('Nie udało się usunąć zaplanowanego zwrotu!');
-            showStatementError($removeRefund);
-        }
-    }
 } else {
     showError('Nie udało się usunąć zakupu!');
     showStatementError($remove);
